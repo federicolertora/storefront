cfe50b8e9ff9dfd5d82445835cfc9ebd538caeb5
Federico Lertora <6621495+federicolertora@users.noreply.github.com>
2017-11-27 16:54:37 +0000

    add search to storefront

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,6 +11,9 @@ class Product < ApplicationRecord
   validates_numericality_of :price, greater_than_or_equal_to: 0.01
   validates_numericality_of :quantity, greater_than_or_equal_to: 0
   
+  def self.search_by_name_or_description(string)
+    where("name LIKE ? OR description LIKE ?", "%#{string}%", "%#{string}%")
+  end
 end
 
 # == Schema Information
