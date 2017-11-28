b376962ec054c3a5ccd511bf14d43dd37ce99c6f
Federico Lertora <6621495+federicolertora@users.noreply.github.com>
2017-11-28 16:54:26 +0000

    add shopping cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,21 @@
 class CartsController < ApplicationController
   
-  incluse CurrentCart
+  include CurrentCart
   before_action :set_cart
   
   def edit
+  end
+  
+  def destroy
+    if @cart.destroy
+      session[:cart_id] = nil
+      flash[:notice] = "Cart was successfully deleted."
+      redirect_to shop_path
+    else
+      flash[:notice] = "Couldn't delete cart."
+      redirect_to edit_cart_path
+    end
+    
   end
   
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,15 @@
 class Cart < ApplicationRecord
     has_many :line_items, dependent: :destroy
+    
+    def subtotal
+        #the ruby way
+        #line_items.to_a.sum { |item| item.total }
+        #end
+        
+        #sql way (much faster!)
+        line_items.select("SUM(quantity *price) AS sum")[0].sum
+        
+    end
 end
 
 # == Schema Information
